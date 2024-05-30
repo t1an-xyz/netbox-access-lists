2a7552aa7b9ed7a02bbf0b757313495dd5f48900
Theo <62950302+t1an-xyz@users.noreply.github.com>
2024-05-30 15:44:53 -0400

    Add search indexing

---
+++ netbox_access_lists/search.py
@@ -0,0 +1,22 @@
+from netbox.search import SearchIndex, register_search
+from .models import AccessList, AccessListRule
+
+@register_search
+class AccessListIndex(SearchIndex):
+    model = AccessList
+    fields = (
+        ('name', 100),
+        ('comments', 5000),
+    )
+
+@register_search
+class AccessListRuleIndex(SearchIndex):
+    model = AccessListRule
+    fields = (
+        ('description', 500),
+    )
+
+indexes = [
+    AccessListIndex,
+    AccessListRuleIndex,
+]
